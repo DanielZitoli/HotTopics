85a28f1625102b666cb3ef9dac6b01f060b7f9d5
DanielZitoli <danielzitoli@rogers.com>
2021-09-01 22:40:21 -0400

    first commit

---
+++ project/static/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $("#openSidebar").click(function(){
+        $("#sidebar").css('left', '0')
+        
+    });
+    $("#closeSidebar").click(function(){
+        $("#sidebar").css('left', '-300px')
+    });
+
+
+
+
+});
