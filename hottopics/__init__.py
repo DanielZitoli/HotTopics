26038e4de955863defab6d77a7af674769cdc5e1
DanielZitoli <danielzitoli@rogers.com>
2022-01-23 17:01:24 -0500

    initial deploy8

--- hottopics/__init__.py
+++ hottopics/__init__.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '055a23834110d872426d5f7ee0ac198a'
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 # Configs cookies to be stored in computer's harddrive, and to not last permanently
@@ -16,10 +15,12 @@ app.config["SESSION_TYPE"] = "filesystem"
 ENV = 'prod'
 if ENV == 'dev':
     app.debug = True
+    app.config['SECRET_KEY'] = '055a23834110d872426d5f7ee0ac198a'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/hottopics'
 else:
     app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yrnugqkbbjqmqd:9af42259e3b62867f9add5761bd5295d29844ac6924232422ab9907012210de5@ec2-34-205-209-14.compute-1.amazonaws.com:5432/d1u80l661238l6'
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', '') 
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
